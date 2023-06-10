dbd982d6e30e872e177c4edb85c97d70f0e4a764
Anna Drewing <anna.drewing@tutanota.com>
2023-06-10 11:38:25 +0200

    new expense object can be added

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,39 @@ import Home from "./home/Home";
 import AddExpense from "./addExpense/AddExpense";
 import CustomizationBar from "./customization/CustomizationBar";
 import ExpenseReport from "./expenseReport/ExpenseReport";
+import { v4 as uuid } from "uuid";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 function App() {
-  //const [count, setCount] = useState(0);
+  const [expenseList, setExpenseList] = useState([]);
+  const addExpense = (date, cat, money) => {
+    console.log(expenseList);
+    console.log("Category is: " + cat);
+    console.log("Price is: " + money);
+    setExpenseList((oldExpenseList) => [
+      ...oldExpenseList,
+      { id: uuid(), date: date, category: cat, price: money },
+    ]);
+  };
 
   return (
-    <div className="App">
-      <CustomizationBar />
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/add" element={<AddExpense />} />
-          <Route exact path="/report" element={<ExpenseReport />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div className="App">
+        <CustomizationBar />
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route
+              exact
+              path="/add"
+              element={<AddExpense addExpense={addExpense} />}
+            />
+            <Route exact path="/report" element={<ExpenseReport />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </LocalizationProvider>
   );
 }
 
--- src/addExpense/AddExpense.jsx
+++ src/addExpense/AddExpense.jsx
@@ -1,12 +1,77 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
 
-export default function AddExpense() {
+export default function AddExpense({ addExpense }) {
   const navigate = useNavigate();
+
+  const [category, setCategory] = useState("No category");
+  const [price, setPrice] = useState(0);
+  const [date, setDate] = useState(dayjs("2023-01-01"));
+
+  const updateCategory = (evt) => {
+    setCategory(evt.target.value);
+  };
+
+  const updatePrice = (evt) => {
+    setPrice(evt.target.value);
+  };
+
+  const updateDate = (newDate) => {
+    console.log(newDate.$d);
+    setDate(newDate);
+  };
+
+  const newExpense = () => {
+    console.log(
+      "Adding new expense of category + " + category + ", and of price " + price
+    );
+    addExpense(date, category, price);
+    //TODO: clear the form fields
+    //TODO: add feedback for user
+  };
 
   return (
     <>
-      <h1 style={{ color: "green" }}>Here you can add your expenses</h1>
+      <h3>Here you can add your expenses</h3>
+      <DatePicker label="Pick the date" onChange={updateDate} value={date} />
+      <TextField
+        label="Price"
+        id="price"
+        name="price"
+        value={price}
+        onChange={updatePrice}
+        InputProps={{
+          endAdornment: <InputAdornment position="end">€</InputAdornment>,
+        }}
+      />
+      <FormControl>
+        <InputLabel id="category-label">Category</InputLabel>
+        <Select
+          labelId="category-label"
+          id="category"
+          value={category}
+          name="category"
+          label="Category"
+          onChange={updateCategory}
+        >
+          <MenuItem value="No category">
+            <em>No category</em>
+          </MenuItem>
+          <MenuItem value={"Rent"}>Rent</MenuItem>
+          <MenuItem value={"Entertainment"}>Entertainment</MenuItem>
+          <MenuItem value={"Food"}>Food</MenuItem>
+        </Select>
+      </FormControl>
+      <button onClick={newExpense}>Add Expense</button>
       <button onClick={() => navigate(-1)}>Back</button>
     </>
   );
